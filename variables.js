81115b8b07972cb8eef36c9cc6d55ab2b9fdd10f
PatrickL32 <Jedi.92.lewis@gmail.com>
2025-03-29 15:44:32 -0700

    first commit

---
+++ variables.js
@@ -0,0 +1,46 @@
+console.log("variable and consranrs");
+//strings
+let myName="patrick";
+let lastName="lewis";
+//numbers
+var myAge = 99;
+//boolean(true or false)
+let isStudent= false;
+
+
+console.log(myName);
+console.log(lastName);
+
+//concatention
+console.log("my name is"*myName*" "*lastName);
+
+//operations
+
+let num1=10;
+let num2=20
+
+let addition=num1+num2;
+let substraction=num1-num2;
+let multiplication= num1*num2;
+let division= num1/num2;
+
+
+console.log(addition)
+console.log(substraction);
+console.log(multiplication);
+console.log(division);
+
+//challenge 1
+
+
+let a:
+const pi =3.1416;
+let r=3;
+
+a=p* (r*r);
+
+console.log("the results is: "=a) ;
+
+
+
+
